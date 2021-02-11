64d8c9a49a86725da1e96d70560eaafed8a8b8c0
Triston Stuart <triston.stuart@icloud.com>
2021-02-11 12:16:32 -0700

    Fixing the code, formating, and 100% score.
    Makes the code a lot more simple and understandable, removed a ton of unnecessary and complex variables.

--- unit7/lesson3/U7L3Q2/U7_L3_Activity_Two.java
+++ unit7/lesson3/U7L3Q2/U7_L3_Activity_Two.java
@@ -4,53 +4,42 @@ public class U7_L3_Activity_Two
 {
 
   // Write the printStatistics method as described in the assignment
-
-public static void printStatistics(ArrayList<Integer> list) {
-  
-  int sum = 0;
-  double avg = 0;
-  int mode = 0;
-  double maxValue = Double.NaN;
-  int counter = 0;
-  int maxCount = 0;
-  boolean infinite = false;
-  ArrayList<Integer> hold = new ArrayList<Integer>();
-  
-  
-  for (int i = 0; i < list.size(); i++) {
-  sum += list.get(i); 
-  counter++;
-  avg += list.get(i);
-} 
-  avg = avg / counter;
-  
-  for (int i = 0; i < list.size(); i++) {
-    int count = 0;
-    for (int j = i; j < list.size(); j++) {
-      if (list.get(j) == list.get(i)) 
-        count++;
-    }
-        if (count == maxCount) {
-            maxValue = Double.NaN;
-            infinite = true;
+  public static void printStatistics(ArrayList<Integer> list) {
+    int sum = 0;
+    double avg = 0;
+    int mode = 0;
+    
+    int mostAppearingNumber = 0;
+    int mostAppearingCount = 0;
+    int noSingleUnlessGreater = 0;
+    
+    for (Integer number : list) {
+      sum += number;
+      int modeCount = 0;
+      for (Integer referenceNumber : list){
+        if (referenceNumber == number){
+          modeCount++;
         }
-        else if (count > maxCount) {
-            maxCount = count;            
-            maxValue = list.get(i);;
-          }
       }
-  
-  
-  System.out.println("Sum: " + sum);
-  System.out.println("Average: " + avg);
-  if(infinite == true)
-  System.out.println("Mode: no single mode");
-  else if(maxCount > 1){
-  mode = (int) maxValue;
-  System.out.println("Mode: " + mode);
-}
-  else 
-  System.out.println("Mode: no single mode");
-}
-
+      if (modeCount > mostAppearingCount){
+        mostAppearingCount = modeCount;
+        mostAppearingNumber = number;
+      }else if (modeCount == mostAppearingCount && number != mostAppearingNumber){
+        noSingleUnlessGreater = modeCount;
+      }
+    } 
+    avg = sum / (double) list.size();
+    
+    System.out.println("Sum: " + sum);
+    System.out.println("Average: " + avg);
+    if (mostAppearingCount <= 1){
+      System.out.println("Mode: no single mode");
+    }else {
+      if (mostAppearingCount > noSingleUnlessGreater){
+        System.out.println("Mode: " + mostAppearingNumber);
+      }else {
+        System.out.println("Mode: no single mode");
+      }
+    }
+  }
 }
